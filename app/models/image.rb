f35a450341002a98aeb144557881274fd2d7ef79
Marcos Mantovani <mantovani.marcos@gmail.com>
2015-06-18 16:04:26 -0300

    Ao excluir registro, exclui também seu diretório na pasta uploads

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,11 @@
 class Image < ActiveRecord::Base
 	mount_uploader :asset, ImageUploader
 	validates :asset, presence: true
+
+	after_destroy :remove_file_directory
+
+  def remove_file_directory
+    path = File.dirname(asset.file.file)
+    FileUtils.remove_dir(path, force: false)
+  end
 end
